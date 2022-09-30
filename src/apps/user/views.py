c60b2962da9614c49f5e9cbde1b3ea5fad8ac920
NuclearHolocauster <vanzhdan@gmail.com>
2022-09-30 22:33:21 +0700

    add auth by pallada

--- src/apps/user/views.py
+++ src/apps/user/views.py
@@ -31,14 +31,39 @@ class ObtainAuthToken(StandartObtainAuthToken):
 
     def __check_user(self, request):
         base_url = settings.AUTH_URL
-        auth = requests.post(
+        session = requests.session()
+        auth = session.post(
             url=f"{base_url}/login/",
             json={
                 "login": request.data['username'],
                 "password": request.data['password']
-                }
+                },
+            headers=settings.AUTH_HEADERS
         )
-        return auth
+        if auth.status_code == 200:
+            full_name = session.post(
+                url=f"{base_url}/me",
+                params={
+                    "token": auth.json()['token']
+                },
+                headers=settings.AUTH_HEADERS
+            )
+            user = User.objects.create(
+                full_name=full_name.json()['full_name'],
+                username=request.data['username']
+            )
+            user.set_password(request.data['password'])
+            user.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return {
+                    "token": token.key,
+                    "role": user.role,
+                    "name": user.full_name,
+                    "id": user.id,
+                }
+        return {
+            "detail": "Неверный логин или пароль"
+        }
 
 
 obtain_auth_token = ObtainAuthToken.as_view()
--- src/core/settings.py
+++ src/core/settings.py
@@ -143,6 +143,15 @@ USE_TZ = True
 
 AUTH_URL = os.environ.get('AUTH_URL', 'https://auth.mysibsau.ru')
 
+AUTH_HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 Firefox/14.0.1',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'Connection': 'keep-alive',
+            'DNT': '1'
+        }
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
