1eabd8ea69c03532ef67461d00f4cc5cb603f34a
Иван Жданов <vanzhdan@gmail.com>
2022-11-17 16:44:07 +0700

    fix(urls.py) fix static files with debug mode off

--- src/core/settings.py
+++ src/core/settings.py
@@ -155,7 +155,7 @@ AUTH_HEADERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, '/resources/static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'resources/static')
